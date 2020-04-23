3f51db0049ea2d4cc605300c9d07f2340714bbbe
sritar99 <sadhutarun00@gmail.com>
2020-04-23 16:51:05 +0530

    Done retrieval of members and searching on them

--- controllers/groupChannel.js
+++ controllers/groupChannel.js
@@ -7,6 +7,9 @@ module.exports = function(Users,async,GroupMsg,Message,FriendResult){
             
             router.post('/group/:name',this.groupPostPage);
 
+            router.get('/members', this.viewMembers);
+            router.post('/members', this.searchMembers);
+
             router.get('/logout',this.logout);
 
             
@@ -100,6 +103,50 @@ module.exports = function(Users,async,GroupMsg,Message,FriendResult){
                 res.redirect('/group/'+req.params.name);
             });
         },
+
+
+        viewMembers: function(req, res){
+            async.parallel([
+                function(callback){
+                    Users.find({}, (err, result) => {
+                       callback(err, result); 
+                    });
+                }
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk  = [];
+                const chunkSize = 4;
+                for (let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('members', {title: "All Friends", user: req.user, chunks: dataChunk});
+            })
+        },
+
+        searchMembers: function(req, res){
+            async.parallel([
+                function(callback){
+                    const regex = new RegExp((req.body.username), 'gi');
+                    
+                    Users.find({'username': regex}, (err, result) => {
+                       callback(err, result); 
+                    });
+                }
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk  = [];
+                const chunkSize = 4;
+                for (let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('members', {title: 'Footballkik - Members', user: req.user, chunks: dataChunk});
+            })
+        },
+
 
 
 
--- controllers/privatechat.js
+++ controllers/privatechat.js
@@ -6,7 +6,11 @@ module.exports = function(async,Users,Message){
         SetRouting: function(router){
             router.get('/chat/:name',this.getchatPage);
 
-            router.post('/chat/:name',this.chatPostPage)
+            router.post('/chat/:name',this.chatPostPage);
+
+
+            router.get('/members', this.viewMembers);
+            router.post('/members', this.searchMembers);
 
         },
 
@@ -139,6 +143,50 @@ module.exports = function(async,Users,Message){
                 res.redirect('/chat/'+req.params.name);
             });
             
+        },
+
+
+
+        viewMembers: function(req, res){
+            async.parallel([
+                function(callback){
+                    Users.find({}, (err, result) => {
+                       callback(err, result); 
+                    });
+                }
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk  = [];
+                const chunkSize = 4;
+                for (let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('members', {title: "All Friends", user: req.user, chunks: dataChunk});
+            })
+        },
+
+        searchMembers: function(req, res){
+            async.parallel([
+                function(callback){
+                    const regex = new RegExp((req.body.username), 'gi');
+                    
+                    Users.find({'username': regex}, (err, result) => {
+                       callback(err, result); 
+                    });
+                }
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk  = [];
+                const chunkSize = 4;
+                for (let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('members', {title: 'Footballkik - Members', user: req.user, chunks: dataChunk});
+            })
         },
 
 
